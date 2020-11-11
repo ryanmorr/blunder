da5ab260dac9445044c763094ff6d00b4c310182
Ryan Morr <rm.morr@gmail.com>
2020-11-10 21:07:49 -0500

    Ensure attempt does not dispatch an error multiple times to global error handlers

--- src/attempt.js
+++ src/attempt.js
@@ -2,13 +2,13 @@ import { Exception } from './exception';
 import { dispatch } from './bus';
 
 export function attempt(fn) {
-    return new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
         try {
             resolve(fn());
         } catch(e) {
-            const error = Exception.from(e);
-            reject(error);
-            dispatch(error);
+            reject(Exception.from(e));
         }
     });
+    promise.catch(dispatch);
+    return promise;
 }
--- test/specs/attempt.js
+++ test/specs/attempt.js
@@ -1,4 +1,4 @@
-import { attempt, subscribe, Exception } from '../../src/blunder';
+import { attempt, subscribe, monitor, Exception } from '../../src/blunder';
 
 describe('attempt', () => {
     it('should resolve when the provided function is successfully executed', (done) => {
@@ -24,7 +24,7 @@ describe('attempt', () => {
         });
     });
 
-    it('should dispatch an error to global error handlers', () => {
+    /* it('should dispatch an error to global error handlers', () => {
         const message = 'An error occurred';
         const error = new Error(message);
 
@@ -43,5 +43,61 @@ describe('attempt', () => {
         expect(e.originalError).to.equal(error);
 
         unsubscribe();
+    }); */
+
+
+
+
+    it('should dispatch an error to global error handlers', (done) => {
+        const message = 'An error occurred';
+        const error = new Error(message);
+
+        const callback = sinon.spy();
+        const unsubscribe = subscribe(callback);
+
+        attempt(() => {
+            throw error;
+        }).catch((ex) => {
+            expect(callback.callCount).to.equal(1);
+
+            expect(callback.args[0][0]).to.equal(ex);
+            expect(ex).to.be.an.instanceof(Exception);
+            expect(ex.message).to.equal(message);
+            expect(ex.originalError).to.equal(error);
+
+            unsubscribe();
+            done();
+        });
+    });
+
+    it('Should dispatch an error only once to the global error handlers', (done) => {
+        let stop, unsubscribe;
+
+        const callback = sinon.spy();
+        unsubscribe = subscribe(callback);
+
+        stop = monitor({error: false, unhandledrejection: false, rejectionhandled: true});
+
+        const promise = attempt(() => {
+            throw new Error('An error occurred');
+        });
+
+        promise.catch((ex) => {
+            window.dispatchEvent(new PromiseRejectionEvent('rejectionhandled', {
+                reason: ex,
+                promise
+            }));
+        });
+
+        const onRejectionHandled = () => {
+            expect(callback.callCount).to.equal(1);
+
+            window.removeEventListener('rejectionhandled', onRejectionHandled);
+            stop();
+            unsubscribe();
+            done();
+        };
+        
+        window.addEventListener('rejectionhandled', onRejectionHandled);
     });
 });
