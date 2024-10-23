af9c6752f1815f46e32559b6b855a5379300402a
Ryan Morr <rm.morr@gmail.com>
2024-10-23 11:30:37 -0400

    Change to use sendBeacon interally

--- src/report.js
+++ src/report.js
@@ -1,19 +1,3 @@
-import { Exception } from './exception';
-
 export function report(url, data) {
-    return new Promise((resolve, reject) => {
-        const fail = (e) => reject(Exception.from(e));
-        fetch(url, {
-            method: 'POST',
-            cache: 'no-cache',
-            credentials: 'include',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        }).catch(fail).then((response) => {
-            if (!response.ok) {
-                return Promise.reject(Exception.from(response.statusText));
-            }
-            return response.json();
-        }).then(resolve).catch(fail);
-    });
+    return navigator.sendBeacon(url, JSON.stringify(data));
 }
--- test/specs/report.js
+++ test/specs/report.js
@@ -1,102 +1,50 @@
 import { report, Exception } from '../../src/blunder';
 
 describe('report', () => {
-    let stubbedFetch = sinon.stub(window, 'fetch');
+    let stubbedSendBeacon = sinon.stub(navigator, 'sendBeacon');
 
     after(() => {
-        stubbedFetch.restore();
+        stubbedSendBeacon.restore();
     });
 
-    it('should send an error to the server', (done) => {
-        const response = {done: true};
-    
-        fetch.returns(Promise.resolve(new Response(JSON.stringify(response), {
-            status: 200,
-            headers: {'Content-type': 'application/json'}
-        })));
+    it('should send an error to the server', () => {
+        navigator.sendBeacon.returns(true);
         
         const error = new Exception();
-        const promise = report('/path/to/endpoint', error);
+        const successful = report('/path/to/endpoint', error);
 
-        expect(promise).to.be.a('promise');
+        expect(successful).to.equal(true);
+    
+        expect(navigator.sendBeacon.callCount).to.equal(1);
+        expect(navigator.sendBeacon.args[0][0]).to.equal('/path/to/endpoint');
+        expect(navigator.sendBeacon.args[0][1]).to.equal(JSON.stringify(error));
+
+        navigator.sendBeacon.reset();
+    });
+
+    it('should send an array of errors to the server', () => {    
+        navigator.sendBeacon.returns(true);
         
-        promise.then((data) => {
-            expect(response).to.deep.equal(data);
+        const errors = [new Exception(), new Exception()];
+        const successful = report('/path/to/endpoint', errors);
+
+        expect(successful).to.equal(true);
     
-            expect(fetch.callCount).to.equal(1);
-            expect(fetch.args[0][0]).to.equal('/path/to/endpoint');
-            expect(fetch.args[0][1]).to.deep.equal({
-                method: 'POST',
-                cache: 'no-cache',
-                credentials: 'include',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(error)
-            });
-    
-            fetch.reset();
-            done();
-        });
+        expect(navigator.sendBeacon.callCount).to.equal(1);
+        expect(navigator.sendBeacon.args[0][0]).to.equal('/path/to/endpoint');
+        expect(navigator.sendBeacon.args[0][1]).to.equal(JSON.stringify(errors));
+
+        navigator.sendBeacon.reset();
     });
 
-    it('should send an array of errors to the server', (done) => {    
-        fetch.returns(Promise.resolve(new Response('{}', {
-            status: 200,
-            headers: {'Content-type': 'application/json'}
-        })));
+    it('should return false if the error could not be sent to the server', () => {    
+        navigator.sendBeacon.returns(false);
         
-        const error1 = new Exception();
-        const error2 = new Exception();
-        report('/path/to/endpoint', [error1, error2]).then(() => {
-            expect(fetch.args[0][1].body).to.equal(JSON.stringify([error1, error2]));
-    
-            fetch.reset();
-            done();
-        });
-    });
-
-    it('should fail if there is a network failure', (done) => {
-        const fetchError = new Error('Could not connect');
-        fetch.returns(Promise.reject(fetchError));
-    
         const error = new Exception();
-        report('/path/to/endpoint', error).catch((e) => {
-            expect(e).to.be.an.instanceof(Exception);
-            expect(e.message).to.equal('Could not connect');
-            expect(e.source).to.equal(fetchError);
-    
-            fetch.reset();
-            done();
-        });
-    });
+        const successful = report('/path/to/endpoint', error);
 
-    it('should fail if the response has an unsuccessful HTTP status', (done) => {
-        fetch.returns(Promise.resolve(new Response(JSON.stringify({}), {
-            status: 500,
-            statusText: 'Internal Server Error'
-        })));
-    
-        const error = new Exception();
-        report('/path/to/endpoint', error).catch((e) => {
-            expect(e).to.be.an.instanceof(Exception);
-            expect(e.message).to.equal('Internal Server Error');
-    
-            fetch.reset();
-            done();
-        });
-    });
+        expect(successful).to.equal(false);
 
-    it('should fail if the response cannot generate a JSON object', (done) => {
-        fetch.returns(Promise.resolve(new Response(null, {
-            status: 200
-        })));
-    
-        const error = new Exception();
-        report('/path/to/endpoint', error).catch((e) => {
-            expect(e).to.be.an.instanceof(Exception);
-            expect(e.message).to.equal('Unexpected end of input');
-    
-            fetch.reset();
-            done();
-        });
+        navigator.sendBeacon.reset();
     });
 });
